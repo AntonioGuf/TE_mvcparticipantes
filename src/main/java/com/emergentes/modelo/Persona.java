96ae269676e1cfbf3ff6e14126a357a03c809a29
Jose Antonio Gutierrez <tomjagkub@gmail.com>
2021-10-13 01:19:28 -0300

    Septimo commit

---
+++ src/main/java/com/emergentes/modelo/Persona.java
@@ -0,0 +1,71 @@
+
+package com.emergentes.modelo;
+
+
+public class Persona {
+    private int id;
+    private String nombres;
+    private String apellidos;
+    private String categoria;
+    private boolean confirmado;
+    private String fecha;
+
+public Persona() {
+        this.id = 0;
+        this.nombres = "";
+        this.apellidos = "";
+        this.categoria = "";
+        this.confirmado = true;
+        this.fecha = "";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isConfirmado() {
+        return confirmado;
+    }
+
+    public void setConfirmado(boolean confirmado) {
+        this.confirmado = confirmado;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+   
+}
